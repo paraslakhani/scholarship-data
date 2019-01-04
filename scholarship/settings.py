d991c458b1cf270cbb3c0939f06fdb5379b99b09
Paras Lakhani <you@example.com>
2019-01-04 13:50:42 +0530

    Remove setting directory

--- scholarship/settings.py
+++ scholarship/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '0zm%d-7-m07^27mc-2-knyvbp=72sf5_o3cb9rzwp77l7q0mm%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['3.83.109.240','localhost','127.0.0.1']
 
 
 # Application definition
--- scholarship/settings/production.py
+++
@@ -1,5 +0,0 @@
-from .base import *
-
-DEBUG = False
-
-ALLOWED_HOSTS = ['3.83.109.240'] # or ['*']
